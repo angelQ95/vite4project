582c6e2cbc563d3497bd6c3e845c07b04bea1906
angelQ95 <angelq1995@163.com>
2023-03-22 17:09:54 +0800

    1:增加“@缩略符”@types/node；2：增加lodash依赖包；3:修正错误问题

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,10 +7,10 @@ store.use(createPersistedState());
 
 export { store };
 
-export * from './modules/notification';
-export * from './modules/permission';
-export * from './modules/user';
-export * from './modules/setting';
-export * from './modules/tabs-router';
+// export * from './modules/notification';
+// export * from './modules/permission';
+// export * from './modules/user';
+// export * from './modules/setting';
+// export * from './modules/tabs-router';
 
 export default store;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
+
+
+const pathResolve = (dir: string): any => {
+  return resolve(__dirname, '.', dir);
+};
+
+const alias: Record<string, string> = {
+  '@': pathResolve('./src'),
+};
+
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // 配置路径别名
+    alias,
+  },
 })
