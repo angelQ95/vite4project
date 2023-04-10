0c89a57c2404767e5350967fa43bae4ec3557086
angelQ95 <angelq1995@163.com>
2023-04-10 17:53:32 +0800

    1:增加pageEnum枚举

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <NConfigProvider v-if="!isLock" :locale="zhCN" :theme="getDarkTheme" :theme-overrides="getThemeOverrides"
+  <NConfigProvider :locale="zhCN" :theme="getDarkTheme" :theme-overrides="getThemeOverrides"
     :date-locale="dateZhCN">
     <AppProvider>
       <RouterView />
     </AppProvider>
   </NConfigProvider>
-  <transition v-if="isLock && $route.name !== 'login'" name="slide-up">
-    <LockScreen />
-  </transition>
 </template>
 <script setup lang="ts">
 import { computed, onMounted, onUnmounted } from 'vue';
 import {zhCN, dateZhCN, darkTheme, NLoadingBarProvider } from 'naive-ui'
 
-import { LockScreen } from '@/components/Lockscreen'
 </script>
 <style lang="less">
 @import 'styles/index.less';
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,20 +1,7 @@
 <template>
-    <n-config-provider :theme-overrides="themeOverrides" :theme="theme" :locale="zhCN" style="height: 100%">
+    <n-config-provider style="height: 100%">
         <n-global-style />
     </n-config-provider>
 </template>
 <script setup lang="ts">
-import useAppConfigStore from '@/store/modules/app-config'
-import { NButton, NLayoutSider, NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter } from 'naive-ui';
-import { computed, defineComponent, onBeforeUnmount, onMounted } from 'vue'
-
-const appConfig = useAppConfigStore();
-const themeOverrides = computed(()=>{
-    return {
-        common: {
-            primaryColor: appConfig.themeColor,
-            primaryColorHover: appConfig.themeColor
-        }
-    }
-})
 </script>
