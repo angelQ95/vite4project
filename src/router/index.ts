7aa02db6b1143b97c90768dace71a20dac082625
angelQ95 <angelq1995@163.com>
2023-04-12 16:35:57 +0800

    添加routes下moudels文件夹，定义各模块路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,12 @@ import { PageEnum } from '@/enums/pageEnum';
 import { createRouterGuards } from './router-guards';
 import type { IModuleType } from './types';
 
+/**
+ * 这段代码使用了 ES2020 中的动态导入语法 import.meta.glob()，它会根据指定的模式匹配符('./modules/...')，
+ * 在指定目录('./modules')下查找所有的 .ts 文件，返回一个对象。
+ * 对象的键是文件的路径，值是一个 Promise 对象，用于异步地加载对应的模块。
+ * 此处还加了一个选项 { eager: true }，表示在导入时立即加载所有匹配的模块。
+ */
 const modules = import.meta.glob<IModuleType>('./modules/**/*.ts', { eager: true });
 
 const routeModuleList: RouteRecordRaw[] = [];
