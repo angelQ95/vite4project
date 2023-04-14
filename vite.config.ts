f4812ed0c933a904b97b18af4d56a3cb9749ce2c
angelQ95 <angelq1995@163.com>
2023-04-14 14:19:46 +0800

    vite配置文件增加mock设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
-
+import { viteMockServe } from 'vite-plugin-mock';
+import { UserConfigExport, ConfigEnv } from 'vite';
 // https://vitejs.dev/config/
 
 
@@ -12,12 +13,33 @@ const pathResolve = (dir: string): any => {
 const alias: Record<string, string> = {
   '@': pathResolve('./src'),
 };
-
-
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    // 配置路径别名
-    alias,
-  },
-})
+//设置mock服务
+export default ({ command }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      viteMockServe({
+        // default
+        mockPath: 'mock',
+        localEnabled: command === 'serve',
+      }),
+    ],
+    resolve: {
+      // 配置路径别名
+      alias,
+    },
+  }
+}
+//基本设置
+// export default defineConfig({
+//   plugins: [vue(),
+//     viteMockServe({
+//       // default
+//       mockPath: 'mock',
+//       localEnabled: command === 'serve',
+//     })],
+//   resolve: {
+//     // 配置路径别名
+//     alias,
+//   },
+// })
