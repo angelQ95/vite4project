3ba37fbae52c43505c169cb3a4b26bf3f82307c1
angelQ95 <angelq1995@163.com>
2023-04-25 10:49:00 +0800

    store文件夹下types文件更新

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,12 +1,105 @@
-import { IAsyncRouteState } from '@/store/modules/asyncRoute';
-import { IUserState } from '@/store/modules/user';
-import { ILockscreenState } from '@/store/modules/lockscreen';
-import { ITabsViewState } from '@/store/modules/tabsView';
+import { Ref, UnwrapRef } from 'vue'
+import { RouteMeta, RouteRecordNormalized, RouteRecordRaw } from 'vue-router'
 
-export interface IStore {
-  asyncRoute: IAsyncRouteState;
-  user: IUserState;
-  lockscreen: ILockscreenState;
-  tabsView: ITabsViewState;
-  count: number;
+export enum LayoutMode {
+  LTR = 'ltr',
+  LCR = 'lcr',
+  TTB = 'ttb',
+}
+
+export enum DeviceType {
+  PC = 'pc',
+  PAD = 'pad',
+  MOBILE = 'mobile',
+}
+
+export enum ThemeMode {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+export enum SideTheme {
+  DARK = 'dark',
+  WHITE = 'white',
+  BLUE = 'blue',
+  IMAGE = 'image',
+}
+
+export enum PageAnim {
+  FADE = 'fade',
+  OPACITY = 'opacity',
+  DOWN = 'down',
+  SCALE = 'scale',
+}
+
+export interface UserState {
+  userId: number
+  token: string
+  roleId: number
+  roles: string[] | null
+  userName: string
+  nickName: string
+  avatar: string
+}
+
+export interface AppConfigState {
+  projectName: string
+  theme: ThemeMode
+  sideTheme: SideTheme
+  themeColor: string
+  layoutMode: LayoutMode
+  deviceType: DeviceType
+  sideWidth: number
+  pageAnim: PageAnim
+  isFixedNavBar: boolean
+  isCollapse: boolean
+  actionBar: {
+    isShowSearch: boolean
+    isShowMessage: boolean
+    isShowRefresh: boolean
+    isShowFullScreen: boolean
+  }
+}
+
+export interface VisitedRouteState {
+  visitedRoutes: RouteRecordNormalized[]
+  affixRoutes: RouteRecordNormalized[]
+}
+
+export interface CachedRouteState {
+  cachedRoutes: string[]
+}
+
+export interface OriginRoute {
+  parentPath?: string
+  menuUrl: string
+  menuName?: string
+  routeName?: string
+  hidden?: boolean
+  outLink?: string
+  affix?: boolean
+  cacheable?: boolean
+  isRootPath?: boolean
+  iconPrefix?: string
+  icon?: string
+  badge?: string | number
+  isSingle?: boolean
+  localFilePath?: string
+  children?: Array<OriginRoute>
+}
+
+export interface RouteMetaType extends RouteMeta {
+  icon?: string
+  title?: string
+  cacheable?: boolean
+  affix?: boolean
+}
+
+export interface SplitTab {
+  label: string
+  iconPrefix?: string | unknown
+  icon: string
+  fullPath: string
+  children?: Array<RouteRecordRaw>
+  checked: Ref<UnwrapRef<boolean>>
 }
