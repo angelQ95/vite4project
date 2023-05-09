c6e6591ed8a32e3ee4d0fcb00690f8888c387e3f
angelQ95 <angelq1995@163.com>
2023-05-09 17:53:09 +0800

    fix(bug):ol文件更改

--- src/pages/cognitive/scene/ol.vue
+++ src/pages/cognitive/scene/ol.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-        dadadadwadawjjjjjs
+    <div id="map">
     </div>
 </template>
 
 <script setup lang="ts">
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    new Map({
+        target: 'map',
+        layers: [
+            new TileLayer({
+                source: new OSM(),
+            })
+        ],
+        view: new View({
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+})
 
 </script>
 
 <style scoped>
+#map {
+    width: 100%;
+    height: 100%;
+}
 </style>
